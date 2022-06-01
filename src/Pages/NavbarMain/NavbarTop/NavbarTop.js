c57506cd5f84e03c55ebb6e4d7ccb664aacede27
Shafique <sfqtuhin04@gmail.com>
2022-06-01 06:42:03 +0600

    navbar updating

---
+++ src/Pages/NavbarMain/NavbarTop/NavbarTop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavbarTop = () => {
+    return (
+        <div className='flex lg:flex-cols md:'>
+           <div></div>
+           <div></div> 
+        </div>
+    );
+};
+
+export default NavbarTop;
