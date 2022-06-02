c30a0e7438939ff2921e5642c7bf35b05723397c
Shafique <sfqtuhin04@gmail.com>
2022-06-02 23:20:13 +0600

    navbar designed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 const Navbar = () => {
   const Links = [
     {
-      icon1: <ion-icon style={{fontSize:'45px',fontWeight:'bold'}}  name="home-outline"></ion-icon>,
-      link: "/home",
+      icon1: <ion-icon style={{fontSize:'35px',fontWeight:'bold'}}  name="home-outline"></ion-icon>,
+      link: "/",
     },
     {
       name: "SEO services",
-      icon1: <ion-icon  name="search-outline"></ion-icon>,
+      icon1: <ion-icon name="search-outline"></ion-icon>,
       link: "/",
     },
     { name: "paid marketing", icon1:<ion-icon name="logo-usd"></ion-icon>,icon2: <ion-icon name="caret-down-outline"></ion-icon>, link: "/" },
@@ -29,7 +29,7 @@ const Navbar = () => {
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static text-white-300 md:z-auto z-[-1] left-0 w-full md:w-auto ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static text-white-300 md:z-auto z-[-1] left-0 w-full md:w-auto last:border-r-0 ${
             open ? "top-20 " : "top-[-490px]"
           }`}
         >
@@ -37,10 +37,10 @@ const Navbar = () => {
             <li
               key={link.name}
               style={{ borderRight: "1px solid white", paddingRight: "8px" }}
-              className="w-48 h-20 text-right font-semibold text-white text-base py-2 hover:bg-primary capitalize"
+              className="w-58 h-20 text-right font-semibold text-white text-base py-2 hover:bg-primary capitalize"
             >
-              <a href={link.link} className="flex items-center justify-center break-words align-middle">
-                <span className="text-3xl my-3">{link.icon1}</span>
+              <a href={link.link} className="flex items-center justify-center break-words">
+                <span className="text-3xl my-3 mx-2">{link.icon1}</span>
                 <span>{link.name}</span>
                 <span className="ml-3 text-3xl">{link.icon2}</span>
               </a>
