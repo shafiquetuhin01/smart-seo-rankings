eb684841c37de0481aadd8483e56dfb7ba3b21c1
Shafique <sfqtuhin04@gmail.com>
2022-06-02 21:44:35 +0600

    navbar updating

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 const Navbar = () => {
   const Links = [
     {
-      icon1: <ion-icon name="home-outline"></ion-icon>,
+      icon1: <ion-icon style={{fontSize:'45px',fontWeight:'bold'}}  name="home-outline"></ion-icon>,
       link: "/home",
     },
     {
-      name: "seo service",
-      icon1: <ion-icon name="search-outline"></ion-icon>,
+      name: "SEO services",
+      icon1: <ion-icon  name="search-outline"></ion-icon>,
       link: "/",
     },
     { name: "paid marketing", icon1:<ion-icon name="logo-usd"></ion-icon>,icon2: <ion-icon name="caret-down-outline"></ion-icon>, link: "/" },
@@ -37,12 +37,12 @@ const Navbar = () => {
             <li
               key={link.name}
               style={{ borderRight: "1px solid white", paddingRight: "8px" }}
-              className="w-48 h-20 text-right font-semibold text-white text-base py-3 hover:bg-primary capitalize"
+              className="w-48 h-20 text-right font-semibold text-white text-base py-2 hover:bg-primary capitalize"
             >
-              <a href={link.link} className="break-words align-middle">
-                {link.icon1}
-                {link.name}
-                {link.icon2}
+              <a href={link.link} className="flex items-center justify-center break-words align-middle">
+                <span className="text-3xl my-3">{link.icon1}</span>
+                <span>{link.name}</span>
+                <span className="ml-3 text-3xl">{link.icon2}</span>
               </a>
             </li>
           ))}
