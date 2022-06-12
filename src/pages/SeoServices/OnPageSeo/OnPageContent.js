53affad70a9031813a5029c559dd4116147e7b93
unknown <sfqtuhin04@gmail.com>
2022-06-12 06:37:45 +0600

    seo page updating

--- src/pages/SeoServices/OnPageSeo/OnPageContent.js
+++ src/pages/SeoServices/OnPageSeo/OnPageContent.js
@@ -1,123 +1,149 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./OnPageSeo.css";
 const OnPageContent = () => {
   return (
     <div className="grid lg:grid-cols-3 gap-10 pb-10">
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/qRLKG67/ba.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Detailed Back-link Audit</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          We look at ALL links generated for a website in order to determine their value and remove spammy links.
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/Jnhhyy4/bpl.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Business Profile Listings</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          These are influential, industry based directories that every business should target to gain traction in Local SEO.
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/b6hjZWM/seo-ca.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">SEO Competitor Analysis</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          We conduct an exhaustive study of competitors in order to identify new link building & marketing opportunities.
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
-      <div className="justify-center items-center">
-        <div className="flex justify-center">
-          <img src="https://i.ibb.co/YfWmGF2/seo-aa.webp" alt="" />
-          <h2 className="text-xl ml-2 font-bold">
-            <Link to="/blog-writing">Website Audit & SEO Assessment</Link>
-          </h2>
+      <div className="flex justify-center">
+        <div >
+          <img
+            className="one-page-image"
+            src="https://i.ibb.co/YfWmGF2/seo-aa.webp"
+            alt=""
+          />
         </div>
-        <div className="ml-2">
-          <p className="ml-16 on-page-content">
-          A critical analysis from the ground up from design infrastructure & content to link profile & brand strength. Get a
+        <div className="ml-2 p-one-page">
+          <h2 className="text-xl font-bold">Website Audit & SEO Assessment</h2>
+          <p className="mt-2">
+            A critical analysis from the ground up from design infrastructure &
+            content to link profile & brand strength. Get a
           </p>
         </div>
       </div>
