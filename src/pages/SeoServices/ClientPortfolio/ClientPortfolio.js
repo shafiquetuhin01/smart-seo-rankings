ee18ae5db9c21a080eaaeb15ddec178c86464946
BiasDesk1 <shafiquetuhin01>
2022-06-15 14:18:41 +0600

    hover effect updated

--- src/pages/SeoServices/ClientPortfolio/ClientPortfolio.js
+++ src/pages/SeoServices/ClientPortfolio/ClientPortfolio.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./ClientPortfolio.css";
 const ClientPortfolio = () => {
+  const [items, setItems] = useState(
+    "https://i.ibb.co/vH4XT3L/automotive.webp"
+  );
+  const changeImage = (items) => {
+    setItems(items);
+  };
   return (
     <div className="border-y py-10 border-black">
       <div className="w-9/12 mx-auto">
@@ -13,7 +21,7 @@ const ClientPortfolio = () => {
             <p className="text-lg">
               Our{" "}
               <span>
-                <a href="#">100% Money Back Guarantee</a>
+                <NavLink to="#">100% Money Back Guarantee</NavLink>
               </span>{" "}
               feature has allowed us to work with thousands of businesses
               worldwide. We have provided comprehensive internet marketing
@@ -24,12 +32,23 @@ const ClientPortfolio = () => {
               <div className="lg:flex mt-7 justify-evenly portfolio-icon items-center gap-5">
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
-                      <img className="img-hover1"
+                    <NavLink to="#">
+                      <img
+                        onMouseEnter={() =>
+                          changeImage(
+                            "https://i.ibb.co/vH4XT3L/automotive.webp"
+                          )
+                        }
+                        onMouseOut={() =>
+                          changeImage(
+                            "https://i.ibb.co/vH4XT3L/automotive.webp"
+                          )
+                        }
+                        className="img-hover1"
                         src="https://i.ibb.co/279SCSz/automotive-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
 
                   <p className="text-center mt-2 text-lg font-bold">
@@ -38,23 +57,36 @@ const ClientPortfolio = () => {
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                  <a href="#">
-                      <img className=" img-hover2"
+                    <NavLink to="#">
+                      <img
+                        onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/x6mpVJ1/business.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/x6mpVJ1/business.webp")
+                        }
+                        className=" img-hover2"
                         src="https://i.ibb.co/qjZGz4t/bussiness-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">Business</p>
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/pddjjNH/construction.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/pddjjNH/construction.webp")
+                        }
                         src="https://i.ibb.co/ByqCVP6/construction-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">
                     Construction
@@ -62,12 +94,18 @@ const ClientPortfolio = () => {
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/3cNPmHv/education.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/3cNPmHv/education.webp")
+                        }
                         src="https://i.ibb.co/rwTvpTw/education-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">
                     Education
@@ -75,12 +113,18 @@ const ClientPortfolio = () => {
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/G776g06/travel.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/G776g06/travel.webp")
+                        }
                         src="https://i.ibb.co/N1DjXkM/travel-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">Travel</p>
                 </div>
@@ -88,24 +132,36 @@ const ClientPortfolio = () => {
               <div className="lg:flex justify-evenly portfolio-icon items-center gap-5 mt-12">
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/VCzbJ3z/health.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/VCzbJ3z/health.webp")
+                        }
                         src="https://i.ibb.co/JzMTY8X/health-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
 
                   <p className="text-center mt-2 text-lg font-bold">Health</p>
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/xzGzGbz/realstate.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/xzGzGbz/realstate.webp")
+                        }
                         src="https://i.ibb.co/X7sdJz2/real-state-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">
                     Real State
@@ -113,23 +169,35 @@ const ClientPortfolio = () => {
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/X8JrCkZ/shopping.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/X8JrCkZ/shopping.webp")
+                        }
                         src="https://i.ibb.co/FD1BRSH/shopping-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">Shopping</p>
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/T4txhhC/technology.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/T4txhhC/technology.webp")
+                        }
                         src="https://i.ibb.co/gPdvq18/technology-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">
                     Technology
@@ -137,12 +205,18 @@ const ClientPortfolio = () => {
                 </div>
                 <div>
                   <div className="portfolio-img-sizing">
-                    <a href="#">
+                    <NavLink to="#">
                       <img
+                      onMouseEnter={() =>
+                          changeImage("https://i.ibb.co/Byz7Mr2/others.webp")
+                        }
+                        onMouseOut={() =>
+                          changeImage("https://i.ibb.co/Byz7Mr2/others.webp")
+                        }
                         src="https://i.ibb.co/v4D44fP/other-sp.webp"
                         alt=""
                       />
-                    </a>
+                    </NavLink>
                   </div>
                   <p className="text-center mt-2 text-lg font-bold">Other</p>
                 </div>
@@ -150,9 +224,17 @@ const ClientPortfolio = () => {
             </div>
           </div>
 
-          <div>
-            <div className="portfolio-img"></div>
-          </div>
+          <div></div>
+          <img
+            style={{
+              width: "457px",
+              height: "445px",
+              objectFit: "cover",
+              
+            }}
+            src={`${items}`}
+            alt=""
+          />
         </div>
       </div>
     </div>
--- src/subPages1/AmazonMarketing.js
+++ src/subPages1/AmazonMarketing.js
@@ -1,17 +1,27 @@
 import React from "react";
-import './AmazonMarketing.css';
+import { useState } from "react";
+import "./AmazonMarketing.css";
 const AmazonMarketing = () => {
-    
+  const [color, setColor]=useState('bg-primary');
+  const handleColor = (change) =>{
+    setColor(change);
+  }
   return (
-    <div>
-      <h2>Amazon Marketing</h2>
-
-      <a class="template-image" href="/template-1978944/editor">
-        <div class="green">Green</div>
-        <div class="red">Red</div>
-        <div class="orange">Orange</div>
-        <div class="image"></div>
-      </a>
+    <div className="mt-12 mx-auto">
+      <div className="text-center text-3xl mb-5 text-indigo-300 flex gap-3 items-center justify-center">
+        <h2 onMouseEnter={()=>handleColor('bg-primary')}
+        onMouseOut={()=>handleColor('bg-primary')}
+         className="hover:bg-purple-500 border p-3 bg-amber-400 text-white border-black">First item Primay</h2>
+        <h2 onMouseEnter={()=>handleColor('bg-secondary')}
+        onMouseOut={()=>handleColor('bg-primary')} className="border p-3 bg-amber-400 hover:bg-slate-600 text-white border-black">Second item Secondary</h2>
+        <h2 onMouseEnter={()=>handleColor('bg-accent')}
+        onMouseOut={()=>handleColor('bg-primary')} className="border p-3 bg-amber-400 hover:bg-cyan-600 text-white border-black">Third item Accent</h2>
+      </div>
+      <div className="flex justify-center items-center gap-5">
+        <div className={`w-96 h-96 ${color}`}></div>
+        
+      </div>
+      
     </div>
   );
 };
