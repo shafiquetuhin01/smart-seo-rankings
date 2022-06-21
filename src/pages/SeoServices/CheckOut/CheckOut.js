693fd132d4957d521bbe1074d1186b749f2153d8
unknown <sfqtuhin04@gmail.com>
2022-06-21 07:22:52 +0600

    featured updating

--- src/pages/SeoServices/CheckOut/CheckOut.js
+++ src/pages/SeoServices/CheckOut/CheckOut.js
@@ -3,40 +3,77 @@ import React from "react";
 const CheckOut = () => {
   return (
     <div className="bg-accent py-10">
-      <div className="w-9/12 mx-auto">
+      <div>
         <h3 className="text-white text-4xl font-bold text-center">
           Check Out Our <span className="text-secondary"> Work Samples!</span>
         </h3>
       </div>
-      <div className="w-10/12 mx-auto">
-        <div className="6/12">
+      <div className="lg:grid grid-cols-2 w-11/12 mx-auto mt-6">
+        <div className="lg:grid grid-cols-2">
           <div
+            className="first-left border-4 border-accent flex-col "
             style={{
-              width: "319px",
-              height: "377px",
+              minHeight: 370,
               backgroundImage: 'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
-            }} className="w-6/12"
+            }}
           >
-            <h3>
+            <div className="mt-5">
               <img
-                className="mx-auto mt-5 pt-5"
+                className="mx-auto"
                 src="https://i.ibb.co/vkBvGcL/web-dd.webp"
                 alt=""
               />
-            </h3>
-            <h3 className="text-white text-lg font-bold mt-12 text-center">
+            </div>
+            <div className="text-white font-bold mt-8 px-1 items-center text-center" style={{fontSize:'18px'}}>
               Website Design & Development
-            </h3>
-            <h3 className="text-center">
-              <button className="btn btn-warning mt-24">view samples</button>
-            </h3>
+            </div>
+            <div className="text-center mt-24">
+              <button className="btn btn-warning">view samples</button>
+            </div>
           </div>
-          <div className="w-6/12">
-            
+          <div className="first-right">
+            <div
+              className="first-right-top border-4 border-accent"
+              style={{
+                minHeight: 185,
+                backgroundImage:
+                  'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
+              }}
+            ></div>
+            <div className="first-right-bottom border-4 border-accent" style={{
+                minHeight: 185,
+                backgroundImage:
+                  'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
+              }}>
+
+            </div>
           </div>
         </div>
+        <div className="second">
+          <div className="second-top border-4 border-accent" style={{
+                minHeight: 185,
+                backgroundImage:
+                  'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
+              }}>
 
-        <div className="w-6/12"></div>
+          </div>
+          <div className="second-bottom lg:grid grid-cols-2">
+            <div className="second-left-top border-4 border-accent" style={{
+                minHeight: 185,
+                backgroundImage:
+                  'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
+              }}>
+
+            </div>
+            <div className="second-right-bottom border-4 border-accent" style={{
+                minHeight: 185,
+                backgroundImage:
+                  'url("https://i.ibb.co/dfnHYqY/bg-sample.png")',
+              }}>
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
