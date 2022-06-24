c80149f28e6f753417b5061222b953dda2861b15
unknown <sfqtuhin04@gmail.com>
2022-06-24 07:53:28 +0600

    section updated

--- src/pages/SeoServices/CheckOut/DesktopFeature.js
+++ src/pages/SeoServices/CheckOut/DesktopFeature.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './DesktopFeature.css';
 const DesktopFeature = () => {
-  const bgImg = "https://i.ibb.co/gZwzWQH/tv.webp ";
+   
   return (
-    <div className="min-h-screen lg:flex w-11/12 mx-auto py-12">
-      <div className="left w-1/2">
+    <div className="min-h-screen lg:flex w-11/12 mx-auto py-20">
+      <div className="left w-full">
         <div className="flex items-center justify-center">
           <img src="https://i.ibb.co/y6B7Pqt/badge.webp" alt="badge" />
           <h3 className="text-white text-4xl font-bold">
@@ -12,37 +12,37 @@ const DesktopFeature = () => {
             <span className="text-secondary">On Google</span>
           </h3>
         </div>
-        <div>
+        <div
+          className="float-right"
+          style={{ marginTop: -60, marginRight: 60 }}
+        >
           <img src="https://i.ibb.co/khB0Yk9/badge-arrow.webp" alt="arrow" />
+        </div>
+        <div>
+          <p className="text-white font-bold text-lg mx-10 mt-3">The keyword rankings highlighted here are a testament to Search Berg’s expertise in Organic SEO. These rankings speak volumes about our expert SEO services and the vast body of expertise we bring to the table.</p>
         </div>
       </div>
       <div
         style={{
-          backgroundImage: `${bgImg}`,
+          backgroundImage: `url("https://i.ibb.co/gZwzWQH/tv.webp")`,
+
+          height: "484px",
+          maxWidth: "100%",
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
-          zIndex:111
-          
         }}
-        className="right w-1/2"
+        className="right w-full p-2 "
       >
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>
-
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><h2>Shafique</h2></td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="text-xl font-bold mt-7 w-8/12 float-right">
+          <h3 className=" inline">Keywords</h3>
+          <h3 className="float-right mr-12">Rankings</h3>
         </div>
+        <div className="relative overflow-y-scroll overflow-item h-64 mr-6 overflow-auto" style={{width:'97%'}} >
+          <img className=" mx-auto" style={{width:540,height:560,maxWidth:"100%"}} src="https://i.ibb.co/kSrt5s1/home-keywords.webp" alt="" />
+        </div>
+       
+        
       </div>
     </div>
   );
