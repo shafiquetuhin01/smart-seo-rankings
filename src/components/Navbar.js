d6a661d623a6398c059c9b9acd7e4defc7145b50
unknown <sfqtuhin04@gmail.com>
2022-07-08 17:07:46 +0600

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,151 +4,100 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const handleClick = () => setOpen(!open);
-  const menuItems = (
-    <>
-      <li className="text-xl">
-        <Link to="/">
-          <Icons.FaSearch />
-          SEO Services
-        </Link>
-      </li>
-      <li className="text-xl" tabIndex="0">
-        <Link to="/paid-marketing">
-          <Icons.FaDollarSign />
-          Paid Marketing
-          <Icons.FaAngleDoubleDown/>
-        </Link>
-        <ul className="bg-primary">
-          <li>
-            <Link to="/google-ads-marketing">Google Ads Marketing</Link>
-          </li>
-          <li>
-            <Link to="/facebook-ads">Facebook Advertising</Link>
-          </li>
-          <li>
-            <Link to="/instagram-marketing">Instagram Marketing</Link>
-          </li>
-          <li>
-            <Link to="/youtube-ads">Youtube Advertising</Link>
-          </li>
-          <li>
-            <Link to="/twitter-marketing">Twitter Marketing</Link>
-          </li>
-          <li>
-            <Link to="/linkedin-marketing">Linkedin Marketing</Link>
-          </li>
-          <li>
-            <Link to="/pinterest-marketing">Pinterest Advertising</Link>
-          </li>
-          <li>
-            <Link to="/amazon-marketing">Amazon Marketing</Link>
-          </li>
-        </ul>
-      </li>
-      <li className="text-xl" tabIndex="0">
-        <Link to="/content-marketing">
-          <Icons.FaBook/>
-          Content Marketing
-          <Icons.FaAngleDoubleDown/>
-        </Link>
-        <ul className="bg-primary">
-          <li>
-            <Link to="/seo-content-writing">SEO Content Writing</Link>
-          </li>
-          <li>
-            <Link to="/article-writing">Article Writing</Link>
-          </li>
-          <li>
-            <Link to="/blog-writing">Blog Writing</Link>
-          </li>
-          <li>
-            <Link to="/email-marketing">Email Marketing</Link>
-          </li>
-          <li>
-            <Link to="/guest-bloging">Guest Bloging</Link>
-          </li>
-        </ul>
-      </li>
-      <li className="text-xl" tabIndex="0">
-        <Link to="/web-design">
-        <Icons.FaDesktop/>
-          Website Design & Development
-          <Icons.FaAngleDoubleDown/>
-        </Link>
-        <ul className="bg-primary">
-          <li>
-            <Link to="/web-design-packages">Web Design Packages</Link>
-          </li>
-          <li>
-            <Link to="/b2b-web-design">B2B Web Design</Link>
-          </li>
-          <li>
-            <Link to="/b2c-web-design">B2C Web Design</Link>
-          </li>
-          <li>
-            <Link to="/ecommerce-web-design">Ecommerce Web Design</Link>
-          </li>
-          <li>
-            <Link to="/wordpress-web-design">WordPress Web Design</Link>
-          </li>
-          <li>
-            <Link to="/responsive-web-design">Responsive Web Design</Link>
-          </li>
-          <li>
-            <Link to="/landing-page-design">Landing Page Design</Link>
-          </li>
-          <li>
-            <Link to="/website-maintenance">Website Maintenance </Link>
-          </li>
-          <li>
-            <Link to="/website-optimization">Website Optimization</Link>
-          </li>
-        </ul>
-      </li>
-    </>
-  );
+
   return (
-    <div
-      style={{
-        backgroundColor: "#091D3F",
-        color: "white",
-        margin: "0",
-        padding: "0",
-      }}
-      className="navbar"
-    >
-      <div className="navbar-start">
-        <div className="dropdown">
-          <ul
-            tabIndex="0"
-            className="menu menu-compact dropdown-content shadow bg-base-100 rounded-box w-52"
-          >
-            {menuItems}
-          </ul>
-        </div>
-      </div>
-      <div className="navbar-center hidden lg:flex z-10">
-        <li className="list-none">
-          <Link to="/internet-marketing">
-            <Icons.FaHome className="flex items-center justify-center text-4xl text-white mr-2 " />
-          </Link>
-        </li>
-        <ul className="menu menu-horizontal custom-ul">{menuItems}</ul>
-      </div>
-      <div className="navbar-end">
+    <div className="z-30 relative">
+      <nav className="">
         <label
-          onClick={handleClick}
-          tabIndex="0"
-          className="nav-icon btn btn-ghost lg:hidden"
+          htmlFor="btn"
+          className="icon z-30 absolute top-[-160px] right-5"
         >
-          <i
-            className={
-              open ? "fas fa-times bg-accent p-2" : "fas fa-bars bg-accent p-2"
-            }
-          ></i>
+          {!open ? (
+            <span onClick={() => setOpen(!open)}>
+              <Icons.FaBars />
+            </span>
+          ) : (
+            <span onClick={() => setOpen(!open)}>
+              <Icons.FaTimes />
+            </span>
+          )}
         </label>
-      </div>
+        <input className="input-box" type="checkbox" id="btn" />
+        <ul className="z-10 m-0">
+          <li className="">
+            <Link to="/">
+              <Icons.FaSearch className="inline mr-2" />
+              SEO Services
+            </Link>
+          </li>
+          <li className="">
+            <label htmlFor="btn-1" className="show">
+              Paid Marketing +
+            </label>
+            <Link to="/paid-marketing">
+              <Icons.FaDollarSign className="inline mr-2" />
+              Paid Marketing
+              <Icons.FaAngleDoubleDown className="inline ml-2" />
+            </Link>
+            <input className="input-box" type="checkbox" id="btn-1" />
+            <ul>
+              <li>
+                <Link to="#">Google Ads Marketing</Link>
+              </li>
+              <li>
+                <Link to="#">Facebook Marketing</Link>
+              </li>
+              <li>
+                <Link to="#">Instagram Marketing</Link>
+              </li>
+            </ul>
+          </li>
+          <li className="">
+            <label htmlFor="btn-2" className="show">
+              Content Marketing +
+            </label>
+            <Link to="/content-marketing">
+              <Icons.FaBook className="inline mr-2" />
+              Content Marketing
+              <Icons.FaAngleDoubleDown className="inline ml-2" />
+            </Link>
+            <input className="input-box" type="checkbox" id="btn-2" />
+            <ul>
+              <li>
+                <Link to="#">SEO Content Writing</Link>
+              </li>
+              <li>
+                <Link to="#">Blog Writing</Link>
+              </li>
+              <li>
+                <Link to="#">Article Writing</Link>
+              </li>
+            </ul>
+          </li>
+          <li className="">
+            <label htmlFor="btn-3" className="show">
+              Web Design & Development +
+            </label>
+            <Link to="/web-design">
+              <Icons.FaDesktop className="inline mr-2"/>
+              Website Design & Development
+              <Icons.FaAngleDoubleDown className="inline ml-2"/>
+            </Link>
+            <input className="input-box" type="checkbox" id="btn-3" />
+            <ul>
+              <li>
+                <Link to="#">Web Design Packages</Link>
+              </li>
+              <li>
+                <Link to="#">B2B Web Design</Link>
+              </li>
+              <li>
+                <Link to="#">B2C Web Design</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
