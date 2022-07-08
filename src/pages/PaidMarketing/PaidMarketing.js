75e56bf1f6343dd6088af82a10424290a0f1f705
unknown <sfqtuhin04@gmail.com>
2022-07-08 10:01:27 +0600

    navbar updating again

--- src/pages/PaidMarketing/PaidMarketing.js
+++ src/pages/PaidMarketing/PaidMarketing.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import PageTitle from '../../components/PageTitle/PageTitle';
+import React from "react";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import { Link } from "react-router-dom";
+import "./PaidMarketing.css";
+import * as Icons from "react-icons/fa";
 
 const PaidMarketing = () => {
   return (
-    <div>
-      <PageTitle title="Paid Marketing"></PageTitle>
-      <h2>Hello world</h2>
-    </div>
+    <div></div>
+    // <div className="mt-10 mb-[50px]">
+    //   <PageTitle title="Paid Marketing"></PageTitle>
+    //   <nav className="z-50">
+    //     <label htmlFor="btn" className="icon">
+    //       <span><Icons.FaBars/></span>
+    //     </label>
+    //     <input type="checkbox" id="btn" />
+    //     <ul>
+    //       <li>
+    //         <Link to="#">SEO Services</Link>
+    //       </li>
+    //       <li>
+    //         <label for="btn-1" className="show">
+    //           Paid Marketing +
+    //         </label>
+    //         <Link to="#">Paid Marketing</Link>
+    //         <input type="checkbox" id="btn-1" />
+    //         <ul>
+    //           <li>
+    //             <Link to="#">Google Ads Marketing</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">Facebook Marketing</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">Instagram Marketing</Link>
+    //           </li>
+    //         </ul>
+    //       </li>
+    //       <li>
+    //         <label for="btn-2" className="show">
+    //           Content Marketing +
+    //         </label>
+    //         <Link to="#">Content Marketing</Link>
+    //         <input type="checkbox" id="btn-2" />
+    //         <ul>
+    //           <li>
+    //             <Link to="#">SEO Content Writing</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">Blog Writing</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">Article Writing</Link>
+    //           </li>
+    //         </ul>
+    //       </li>
+    //       <li>
+    //       <label for="btn-3" className="show">
+    //       Web Design & Development +
+    //         </label>
+    //         <Link to="#">Web Design & Development</Link>
+    //         <input type="checkbox" id="btn-3" />
+    //         <ul>
+    //           <li>
+    //             <Link to="#">Web Design Packages</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">B2B Web Design</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="#">B2C Web Design</Link>
+    //           </li>
+    //         </ul>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    // </div>
   );
 };
 
