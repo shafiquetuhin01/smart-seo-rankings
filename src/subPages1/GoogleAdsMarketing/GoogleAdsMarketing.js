a7fe7ac3d1dc0aad74b43db7b1975ad4a0c8b211
unknown <sfqtuhin04@gmail.com>
2022-07-18 06:56:34 +0600

    google ads page updated

--- src/subPages1/GoogleAdsMarketing/GoogleAdsMarketing.js
+++ src/subPages1/GoogleAdsMarketing/GoogleAdsMarketing.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PageTitle from "../../components/PageTitle/PageTitle";
+import HelpedClient from "../../pages/PaidMarketing/HelpedClient/HelpedClient";
+import PaidAnalytics from "../../pages/PaidMarketing/PaidAnalytics/PaidAnalytics";
+import PaidCarousel from "../../pages/PaidMarketing/PaidCarousel/PaidCarousel";
+import PaidHero from "../../pages/PaidMarketing/PaidHero/PaidHero";
+import PaidHire from "../../pages/PaidMarketing/PaidHire/PaidHire";
+import PaidOptimization from "../../pages/PaidMarketing/PaidOptimization/PaidOptimization";
+import PaidWhyChoose from "../../pages/PaidMarketing/PaidWhyChoose/PaidWhyChoose";
+import PPCStrategy from "../../pages/PaidMarketing/PPCStrategy/PPCStrategy";
+import Awards from "../../pages/SeoServices/Awards/Awards";
 
 const GoogleAdsMarketing = () => {
   return (
-    <div className="mt-10">
+    <div>
       <PageTitle title="Google Ads Marketing"></PageTitle>
-      <h3>This is google ads page</h3>
+      <PaidHero />
+      <Awards />
+      <PPCStrategy />
+      <PaidCarousel />
+      <HelpedClient />
+      <PaidWhyChoose />
+      <PaidOptimization />
+      <PaidAnalytics />
+      <PaidHire />
     </div>
   );
 };
