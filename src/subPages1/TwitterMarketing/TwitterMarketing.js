9291ae6e1c285e8958af58c43e7f340083749edb
unknown <sfqtuhin04@gmail.com>
2022-07-24 19:44:41 +0600

    twitter page updated

--- src/subPages1/TwitterMarketing/TwitterMarketing.js
+++ src/subPages1/TwitterMarketing/TwitterMarketing.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import ContentExperience from '../../pages/ContentMarketing/ContentExperience/ContentExperience';
+import ClientTestimonial from '../../pages/SeoServices/ClientTestimonial/ClientTestimonial';
 import YouTubeAwards from '../YoutubeAdvertising/YouTubeAwards';
+import YouTubeFAQ from '../YoutubeAdvertising/YouTubeFAQ';
+import YouTubeFeaturedContent from '../YoutubeAdvertising/YouTubeFeaturedContent';
+import YouTubeTestimonial from '../YoutubeAdvertising/YouTubeTestimonial';
+import YouTubeVision from '../YoutubeAdvertising/YouTubeVision';
 import TwitterCarousel from './TwitterCarousel';
 import TwitterFollower from './TwitterFollower';
 import TwitterHero from './TwitterHero';
+import TwitterInvest from './TwitterInvest';
 import TwitterTypesOf from './TwitterTypesOf';
 import WhyTwitterImportant from './WhyTwitterImportant';
 import WhyTwitterOffer from './WhyTwitterOffer';
+import YouTubeSign from './YouTubeSign';
 
 const TwitterMarketing = () => {
     return (
@@ -19,6 +27,14 @@ const TwitterMarketing = () => {
             <WhyTwitterOffer/>
             <TwitterCarousel/>
             <TwitterTypesOf/>
+            <TwitterInvest/>
+            <YouTubeTestimonial/>
+            <YouTubeFeaturedContent/>
+            <YouTubeFAQ/>
+            <YouTubeSign/>
+            <YouTubeVision/>
+            <ContentExperience/>
+
         </section>
     );
 };
--- src/subPages1/TwitterMarketing/YouTubeSign.js
+++ src/subPages1/TwitterMarketing/YouTubeSign.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-const YouTubeDontTell = () => {
-  return (
-    <section className="w-11/12 mx-auto py-6">
+const YouTubeSign = () => {
+    return (
+        <section className="w-11/12 mx-auto py-6">
       <div>
         <h3 className="text-info text-4xl font-bold my-3 text-center">
           Don’t Let a Month of Bad Sales Turn Into a Year… Let{" "}
@@ -38,39 +38,19 @@ const YouTubeDontTell = () => {
           </p>
         </div>
         <div className="w-1/2 mx-auto">
-          <img src="https://i.ibb.co/bmRHr3W/fb-advertising.webp" alt="" />
+          <img src="https://i.ibb.co/4p8sq9N/twitter-img4.png" alt="" />
         </div>
       </div>
       <div>
         <p className="text-lg text-justify my-3">
           They target highly specific audiences who frequently engage with a
           specific form of content. Whether your audience comprises tech
-          experts, beauty aficionados, or fitness enthusiasts, YouTube expertly
-          gets your ads across to your target demographic.
+          experts.
         </p>
-        <p className="text-lg text-justify my-3">
-          The outcome? Impressive reach, improved audience engagement and
-          retention, and winning sales that can’t be replicated by other
-          strategies. YouTube display advertising will help you get seen by your
-          target audience and make impressive sales.
-        </p>
-        <p className="text-lg text-justify my-3">
-          At Search Berg, we help clients capitalize YouTube’s lucrative demand
-          and viewership. From creating compelling ads to ensuring seamless ad
-          management over time, our YouTube Ads specialistsgo the extra mile to
-          deliver excellent results. If you’re looking for power-packed YouTube
-          channel promotion, we’re your go-to guys. We take care of YouTube ads
-          for small businesses, startups, and established corporations.
-        </p>
-        <p className="text-lg text-justify my-3">
-          Will you be our next success story? Sign up for our YouTube Ads
-          management and marketing services today! We’re considered one of the
-          top marketing agencies across the globe so you can rest assured that
-          you’re making the right decision.
-        </p>
+        
       </div>
     </section>
-  );
+    );
 };
 
-export default YouTubeDontTell;
+export default YouTubeSign;
